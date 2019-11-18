80168aaa2718d5b420dd1a25e29fa40d69289b7f
Dirk Toewe <DirkToewe@GoogleMail.com>
2019-11-18 18:41:46 +0100

    Finished up release 1.0.0

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
 'use strict';
 
-module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: ['@babel/transform-runtime'],
-  ignore: ['**/*_test.js']
+const glob = require('glob');
+
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/preset-env'],
+    plugins: ['@babel/transform-runtime'],
+    only: glob.sync("src/**/*.js").filter(
+      path => !path.endsWith('_test.js') &&
+              !path.endsWith('_test_data.js')
+    ),
+    sourceType: 'module',
+    sourceMaps: false,
+
+// TODO: get source maps to work and enable the following configs:
+//    sourceMaps: 'inline',
+//    minified: true,
+//    comments: false,
+  };
 };
